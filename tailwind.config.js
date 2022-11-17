5f61925cf579384c661b8a2d02289b3810284763
raihanatllh <raihanatallah52@gmail.com>
2022-11-17 13:30:47 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'birumd':'#d1d1fd',
+        'birumd2':'#282963',
+        'birumd3':'#f0f0fe',
+        'birumd4':'#4e43ac',
+        'birumd5':'#826ff9',
+        'birumd6':'#ac9ffb',
+        'birumd7':'#d1d1fd',
+        'slate':'#A4ADB7'
+      }
+    },
+  },
+  plugins: [],
+}
